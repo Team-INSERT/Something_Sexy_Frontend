a85d562ee79ec87e5771f332da08ca6ce9e49304
youngeunkim <pkhj286@gmail.com>
2023-09-28 17:39:15 +0900

    chore : index -> id

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -4,21 +4,25 @@ import postImg from "../../assets/post.png";
 function Main() {
   const post = [
     {
+      id: 1,
       title: "제목1",
       subTitle: "부제목1",
       button: "신인",
     },
     {
+      id: 2,
       title: "제목2",
       subTitle: "부제목2",
       button: "신인",
     },
     {
+      id: 3,
       title: "제목2",
       subTitle: "부제목2",
       button: "신인",
     },
     {
+      id: 4,
       title: "제목2",
       subTitle: "부제목2",
       button: "신인",
@@ -29,8 +33,8 @@ function Main() {
     <S.MainLayout>
       <S.MainContainer>
         <S.Main>
-          {post.map((item, index) => (
-            <S.Post key={index}>
+          {post.map((item, id) => (
+            <S.Post key={id}>
               <S.PostPicture src={postImg} />
               <S.PostTitle>{item.title}</S.PostTitle>
               <S.PostSubtitle>{item.subTitle}</S.PostSubtitle>
